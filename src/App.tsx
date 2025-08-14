38f7843f9ee03e170d6464ac26b1badc46c3097e
Sarthak Agarwal <sarthakroxxx@gmail.com>
2025-08-14 16:48:52 +0530

    setup react router and added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/home";
+import SignIn from "./pages/sign-in";
+import SignUp from "./pages/sign-up";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
